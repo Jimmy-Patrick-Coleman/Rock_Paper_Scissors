7cc61fe4d365e977adeec12c032b3c2733a3a196
Spoon <JimmyPColeman@outlook.com>
2021-04-16 12:19:42 +0100

    the score is working and ends after rounds i also made the buttons inside images i added with css

--- newscript.js
+++ newscript.js
@@ -15,15 +15,20 @@ let computerPlay = () => {
     return moves[2];
   }
 };
-function resetFunction() {
-
+function restart() {
+  location.reload(); 
+}
+let rounds = 0;
+function roundOver() {
+rounds++;
 }
 
-
-let finsihedRounds = 0;
-function roundOver() {
-  console.log(finsihedRounds,'finsihedRounds')
-  finsihedRounds++;
+function gameOver() {
+  if (playerWins >= 6 ) {
+  restart();
+} else if (computerWins >= 6) {
+  restart();
+}
 }
 
 let playerWins = 0;
@@ -32,7 +37,7 @@ function win() {
   playerWinDiv.textContent = `The player has one ${playerWins} rounds.`
   console.log(playerWins, 'Player wins')
   announceWinner();
-  roundOver();
+  gameOver();
 }
 
 let computerWins = 0;
@@ -40,15 +45,16 @@ function lose() {
   computerWins++;
   computerWinDiv.textContent = `The computer has one ${computerWins} rounds.`
   console.log(computerWins, 'Computer Wins')
-  roundOver();
+  gameOver();
   announceWinner();
 }
 function announceWinner() {
 if (playerWins === 5 ||  computerWins === 5) {
   if (playerWins > computerWins) {
-    gameScore.textContent = 'you win';
+    gameScore.textContent = 'VICTORY press any button to restart';
   } else if (playerWins < computerWins) {
-    gameScore.textContent = 'you lose';
+    gameScore.textContent = 'DEFEAT press any button to restart';
+
   }
 }
 }
@@ -58,65 +64,78 @@ if (playerWins === 5 ||  computerWins === 5) {
       console.log(computerSelection);
       console.log(playerSelection);
       if (playerSelection === "scissors" && computerSelection === "paper") {
-        win()
+        win();
+        roundOver();
         content.textContent = "You Win! Scissors beats Paper";
       } else if (playerSelection === "paper" && computerSelection === "rock") {
         win();
+        roundOver();
         content.textContent = "You Win! Paper beats Rock";
       } else if (
         playerSelection === "rock" &&
         computerSelection === "scissors"
       ) {
         win();
+        roundOver();
         content.textContent = "You Win! Rock beats Scissors";
       } else if (playerSelection === "rock" && computerSelection === "paper") {
         lose();
+        roundOver();
         content.textContent ="You Lose! Paper beats Rock";
       } else if (
         playerSelection === "paper" &&
         computerSelection === "scissors"
       ) {
         lose();
+        roundOver();
         content.textContent = "You Lose! Scissors beats Paper";
       } else if (
         playerSelection === "scissors" &&
         computerSelection === "rock"
       ) {
         lose();
+        roundOver();
         content.textContent = "You Lose! Rock beats Scissors";
       } else if (playerSelection === "rock" && computerSelection === "rock") {
+        roundOver();
         content.textContent = "Tie! You both chose Rock";
       } else if (playerSelection === "paper" && computerSelection === "paper") {
-
+        roundOver();
         content.textContent = "Tie! You both chose Paper";
       } else if (
         playerSelection === "scissors" &&
         computerSelection === "scissors"
       ) {
+        roundOver();
         content.textContent = "Tie! You both chose Scissors";
       } else {
-        content.textContent = "error";
+        return 'ERROR';
       }
     }
-   
-        const score = document.querySelector('#score-div');
-
-        const gameScore = document.createElement('div');
+    const ending = document.querySelector('#ending');
+    const gameScore = document.createElement('div');
         gameScore.classList.add('gameScore');
         gameScore.textContent = '';
-        score.appendChild(gameScore);
+        ending.appendChild(gameScore);
         
+        const score = document.querySelector('#score-div');
 
+        const playerWinDiv = document.createElement('div');
+        playerWinDiv.classList.add('playerWinDiv');
+        playerWinDiv.textContent = `The player has won ${playerWins} rounds.`;
+        score.appendChild(playerWinDiv);
+        
+        const roundsDiv = document.createElement('div');
+        roundsDiv.classList.add('roundsDiv');
+        roundsDiv.textContent = `You have played ${rounds} rounds.`;
+        score.appendChild(roundsDiv);
     
         const computerWinDiv = document.createElement('div');
         computerWinDiv.classList.add('computerWinDiv');
-        computerWinDiv.textContent = `The computer has one ${computerWins} rounds.`;
+        computerWinDiv.textContent = `The computer has won ${computerWins} rounds.`;
         score.appendChild(computerWinDiv);
     
-        const playerWinDiv = document.createElement('div');
-        playerWinDiv.classList.add('playerWinDiv');
-        playerWinDiv.textContent = `The player has one ${playerWins} rounds.`;
-        score.appendChild(playerWinDiv);
+      
     
         
     
@@ -124,15 +143,8 @@ if (playerWins === 5 ||  computerWins === 5) {
         const container = document.querySelector('#result-div');
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = '';
+        content.textContent = 'Are you ready?';
         container.appendChild(content);
-
-     
-
- 
-    
-    
-
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll('button');
   // we use the .forEach method to iterate through each button
